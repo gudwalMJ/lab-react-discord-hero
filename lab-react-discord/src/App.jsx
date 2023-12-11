1948ddd6e22b5b8ec6dfc7371b62325f5b446d46
Josip Milan <josip.milan@aol.com>
2023-12-11 15:04:05 +0100

    discord

---
+++ lab-react-discord/src/App.jsx
@@ -0,0 +1,24 @@
+// src/App.jsx
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/src/assets/discordlogo.png" alt="Discord Logo" />
+      <img src="/src/assets/menuicon.png" alt="Menu Icon" />
+      <h1>
+        IMAGINE A <span class="h1Break">PLACE...</span>
+      </h1>
+      <p>
+        ...where you can belong to a school club, a gaming group, or a worldwide
+        art community. Where just you and a handful of friends can spend time
+        together. A place that makes it easy to talk every day and hang out more
+        often.
+      </p>
+      <button class="macButton">Download for Mac</button>
+      <button class="browserButton">Open Discord in your browser</button>
+    </div>
+  );
+}
+
+export default App;
